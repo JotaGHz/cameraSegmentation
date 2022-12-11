1e87601fb08d9abb5904a6ad3e716bdf6b6f66c0
JotaGHz <jota@Jota2.localdomain>
2022-12-11 20:56:09 -0300

    Add masking of hands

--- projeto.py
+++ projeto.py
@@ -1,12 +1,20 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-height, width = 360, 640
-# For webcam input:
+
 cap = cv2.VideoCapture(0)
+success, image = cap.read()
+
+
+height, width, _ = image.shape
+
+masked = np.zeros((height, width, 3), np.uint8)
+
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
@@ -15,53 +23,65 @@ with mp_hands.Hands(
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+    image = cv2.flip(image, 1)
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
     
-    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
-    x_min, x_max, y_min, y_max = 720, 0, 480, 0
+    x_min, x_max, y_min, y_max = 480, 0, 360, 0
     
+    
+    hull_list = []
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+        #mp_drawing.draw_landmarks(
+        #    image,
+        #    hand_landmarks,
+        #    mp_hands.HAND_CONNECTIONS,
+        #    mp_drawing_styles.get_default_hand_landmarks_style(),
+        #    mp_drawing_styles.get_default_hand_connections_style())
+        
+        for i in range(0, len(hand_landmarks.landmark)):
+            pt = hand_landmarks.landmark[i]
+            hull_list.append((int(pt.x*width), int(pt.y*height)))
+            #cv2.circle(image, (int(pt.x * width), int(pt.y * height)), 5, (0,0,255))
     
-        for mark in hand_landmarks.landmark:
-            x = mark.x * width
-            y = mark.y * height
-            
-            if x > x_max:
-                x_max = int(x)
-            elif x < x_min:
-                x_min = int(x)
-            if y > y_max:
-                y_max = int(y)
-            elif y < y_min:
-                y_min = int(y)
+    
+    if hull_list is not None and len(hull_list) > 0:
+        hull_np = np.array(hull_list)
+        
+        masked = np.zeros(image.shape[:2], np.uint8)
+        
+        convexhull = cv2.convexHull(hull_np)
+        
+        
+        #cv2.polylines(masked, [convexhull], True, (255,255, 255), 3)
+        cv2.fillConvexPoly(masked, convexhull, (255, 255, 255))
+        cv2.polylines(image, [convexhull], True, (0, 255, 0))
+        
+        image = cv2.resize(image, (width, height))
 
-    roi = image[y_min - 10: y_max + 10, x_min - 10: x_max + 10]
-            
-    cv2.rectangle(image, (x_min - 10, y_min - 10), (x_max + 10, y_max + 10), (255, 0, 0), 1)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        masked = cv2.resize(masked, (width, height))
+
+        masked_image = cv2.bitwise_and(image, image, mask = masked)
+        
+        image = cv2.bitwise_and(gray, gray, mask = masked)
+        
+        gray = np.stack((gray,)*3, axis = -1)
+        
+        added_img = cv2.add(masked_image, gray)
+        
+        cv2.imshow('added', added_img)
+
+    cv2.imshow('MediaPipe Hands', image)
     
     
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    
-    
-    #cv2.imshow('Mask', cv2.flip(mask, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
 
